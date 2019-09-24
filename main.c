4cd640322505fc624c305ebe663a38280f961396
Synaxis <01xtor@gmail.com>
2019-09-24 20:55:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,8 +9,7 @@
 #define SA struct sockaddr
 
 //connection between client and server
-
-void func(int sockfd) {
+.//............9%2B..3.19.5.0+(3.19.05.0)..Jm..Apr+19+2013+13%3A19%3A43.....+bfwest-pc...0....4...%3A...125170..ir..1.126.254667.0..9%2B.+8.18.6.0......prod..+d.............e..battlefield-heroes-pc...t..Windows..%2B...standardSecure_v3.&oq=.............9%2B..3.19.5.0+(3.19.05.0)..Jm..Apr+19+2013+13%3A19%3A43.....+bfwest-pc...0....4...%3A...125170..ir..1.126.254667.0..9%2B.%098.18.6.0......prod..+d.............e..battlefield-heroes-pc...t..Windows..%2B...standardSecure_v3void func(int sockfd) {
     char buff[MAX]
     int n;
     //initiate loop for chat
