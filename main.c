0ae2b7a1d3a9bd298eb755c05344058d7b487471
Synaxis <oiacow@gmail.com>
2019-09-24 23:38:50 -0300

    begin

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#define MAX 8096
+#define PORT 51383
+#define SA struct sockaddr
+
+//connection between client and server
+
+void func(int sockfd) {
+    char buff[MAX]
+    int n;
+    //initiate loop for chat
+    for (;;) {
+        bzero(buff, MAX);
+        //
+    }
+
+}
